117dfa7f011b0d6af88ccf3e1c527f2b5ee28d73
Bayron Guerrero Ruiz <bayrongue78@gmail.com>
2024-06-10 21:42:21 -0600

    Se agregan los mensajes que verá el usuario

--- main.c
+++ main.c
@@ -7,3 +7,19 @@ int main() {
     push(stack, 12);
     push(stack, 15);
     push(stack, 37);
+
+    printf("Elemento en la cima: %d\n", peek(stack));
+
+    printf("Elemento sacado: %d\n", pop(stack));
+    printf("Elemento en la cima: %d\n", peek(stack));
+
+    printf("Elemento sacado: %d\n", pop(stack));
+    printf("Elemento sacado: %d\n", pop(stack));
+    
+    // Intentar hacer pop en una pila vacía
+    printf("Elemento sacado: %d\n", pop(stack)); 
+
+    freeStack(stack);
+
+    return 0;
+}
